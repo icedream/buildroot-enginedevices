a6b96e070e57354b427054577f172f277ed4f952
Carl Kittelberger <icedream@icedream.pw>
2022-06-09 06:35:39 +0200

    mixxx: Update dependency list.

--- package/mixxx/mixxx.mk
+++ package/mixxx/mixxx.mk
@@ -3,6 +3,7 @@ MIXXX_SOURCE = mixxx-$(MIXXX_VERSION).tar.gz
 MIXXX_SITE = https://github.com/mixxxdj/mixxx/archive/refs/tags/$(MIXXX_VERSION).tar.gz
 MIXXX_INSTALL_STAGING = NO
 MIXXX_INSTALL_TARGET = YES
+MIXXX_LICENSE = GPLv2
 # MIXXX_CONF_OPTS = -DCMAKE_INSTALL_PREFIX=/usr
 # MIXXX_DEPENDENCIES = \
 # 	protobuf vamp-plugin-sdk rubberband soundtouch \
@@ -11,12 +12,38 @@ MIXXX_INSTALL_TARGET = YES
 # 	libshout libsndfile portmidi portaudio \
 # 	sqlite upower lilv libopenmpt-modplug \
 # 	qt5-declarative qtkeychain-qt5 qt5-svg
+
+# Dependency list put together from
+# 1. https://github.com/mixxxdj/mixxx/wiki/Compiling-On-Linux#arch--derivatives
+# 2. https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=mixxx-git
 MIXXX_DEPENDENCIES = \
 	protobuf libsoundtouch \
 	libid3tag taglib \
-	lame libogg libmad libvorbis mp4v2 faad2 opusfile wavpack \
+	libogg libmad mp4v2 faad2 opusfile \
 	libshout libsndfile portaudio \
 	sqlite upower \
-	qt5
+	qt5 qt5declarative qt5x11extras \
+	\
+	libebur128
+
+ifeq ($(BR2_PACKAGE_LAME),y)
+MIXXX_DEPENDENCIES += lame
+endif
+
+ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
+MIXXX_DEPENDENCIES += libvorbis
+endif
+
+ifeq ($(BR2_PACKAGE_LIBTHEORA),y)
+MIXXX_DEPENDENCIES += libtheora
+endif
+
+ifeq ($(BR2_PACKAGE_LIBEBUR128),y)
+MIXXX_DEPENDENCIES += libebur128
+endif
+
+ifeq ($(BR2_PACKAGE_WAVPACK),y)
+MIXXX_DEPENDENCIES += wavpack
+endif
 
 $(eval $(cmake-package))
