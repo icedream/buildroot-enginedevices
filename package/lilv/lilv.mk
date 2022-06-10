04422d086a81f86bd59443281327b51d7c295d85
Carl Kittelberger <icedream@icedream.pw>
2022-06-10 22:05:07 +0200

    Add sratom package.

--- package/lilv/lilv.mk
+++ package/lilv/lilv.mk
@@ -3,6 +3,6 @@ LILV_SOURCE = lilv-$(LILV_VERSION).tar.bz2
 LILV_SITE = https://download.drobilla.net
 LILV_INSTALL_STAGING = YES
 LILV_LICENSE = ISC
-LILV_DEPENDENCIES = lv2 serd sord
+LILV_DEPENDENCIES = lv2 serd sord sratom
 
 $(eval $(waf-package))
