059e21653d931ced183525edb61c6ea3c35e8714
Carl Kittelberger <icedream@icedream.pw>
2022-12-17 20:26:05 +0100

    Update buildroot config and customizations for Buildroot 2022.02.6.
    The newest firmware now uses the newer Buildroot which ships many
    packages that were not shipped in the past.
    
    Also a lot of new options that we basically just have to acknowledge
    their existence of in the new config.

--- package/lilv/lilv.mk
+++
@@ -1,8 +0,0 @@
-LILV_VERSION = 0.24.20
-LILV_SOURCE = lilv-$(LILV_VERSION).tar.bz2
-LILV_SITE = https://download.drobilla.net
-LILV_INSTALL_STAGING = YES
-LILV_LICENSE = ISC
-LILV_DEPENDENCIES = lv2 serd sord sratom
-
-$(eval $(waf-package))
--- package/serd/serd.mk
+++
@@ -1,7 +0,0 @@
-SERD_VERSION = 0.30.16
-SERD_SOURCE = serd-$(SERD_VERSION).tar.bz2
-SERD_SITE = https://download.drobilla.net
-SERD_INSTALL_STAGING = YES
-SERD_LICENSE = ISC
-
-$(eval $(waf-package))
